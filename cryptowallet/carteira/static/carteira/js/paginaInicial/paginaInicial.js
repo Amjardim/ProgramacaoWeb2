032fa5c63d3eb6c587a0eac13799e281b598a93a
duchometson <ducho@hotmail.com>
2021-11-13 20:30:14 -0300

    Implementando table de moedas front-end

--- cryptowallet/carteira/static/carteira/js/paginaInicial/paginaInicial.js
+++ cryptowallet/carteira/static/carteira/js/paginaInicial/paginaInicial.js
@@ -1,15 +1,59 @@
-onload = function() {  
-    var removeButtons = document.getElementByClass('table-remove');
-    for( var i = 0; i < removeButtons.length; i++ ) {
-        (function(index) {
-            removeButtons[index].addEventListener('click', remove);
-        })(i);
-    }
+var genericDeleteButton = "<td><button id='%IDNAME%'>Delete</button></td>"
+
+
+onload = function() {
+    
+    // configura botoes adicionar
+    document.getElementById('idAdicionar').addEventListener('click', adicionarMoeda);
+    
+    // carrega moedas
+    
+    // configura botoes de deletar
+    configuraBotoesDeletar();
 }
 
-function remove(i) {
-    var removeButtons = document.getElementByClass('table-remove');
-    removeButtons[i].parents('tr').detach();
+function adicionarMoeda() {
+    var table = document.getElementById("idCarteiraMoedas");
+    var formDataMoedaNova = getFormDataMoedaNova();
+    var row = table.insertRow(table.length);
+    row.id = formDataMoedaNova.nome;
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = formDataMoedaNova.nome;
+    cell2.innerHTML = formDataMoedaNova.qtd;
+    cell3.innerHTML = formDataMoedaNova.valor;
+    cell4.innerHTML = formDataMoedaNova.deleteButton;
+    configuraBotoesDeletar();
+}
+
+function getFormDataMoedaNova() {
+    var nome = 'BTC';
+    var moedaNovaDict = {
+        'nome' : nome,
+        'qtd'  : '10' ,
+        'valor':'3.507.241 BRL',
+        'deleteButton' : genericDeleteButton.replace('%IDNAME%','idDelete'+nome)
+    };
+    return moedaNovaDict;
+}
+
+function configuraBotoesDeletar() {
+    var table = document.getElementById('idCarteiraMoedas');
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        var nome = row.innerText.split('\t')[0];
+        var botaoDelete = document.getElementById('idDelete'+nome);
+        console.log(nome);
+        botaoDelete.addEventListener('click',removeMoeda);
+    }    
+}
+
+function removeMoeda(botao) {
+    var nome = botao.target.id.replace('idDelete','');
+    var table = document.getElementById('idCarteiraMoedas');
+    var row = document.getElementById(nome);
+    row.remove();
 }
 
 // const newTr = "
