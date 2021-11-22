8fbd2df66ac93a36ae21fb074eeb914c61978b41
antoniomjardim <antoniomjardim@tecgraf.puc-rio.br>
2021-11-22 14:08:11 -0300

    teste heroku3

---
+++ autenticador/static/autenticador/js/login.js
@@ -0,0 +1,60 @@
+onload = function() {
+    var nome = temNomeUsuarioUrl();
+    if( nome != false ) {
+        document.getElementById('idUsuario').value = nome;
+    }
+    document.getElementById('idLogin').addEventListener('click', loginUsuario);
+    document.getElementById('idRegistrar').addEventListener('click', registrarUsuario);
+}
+
+function loginUsuario(evento) {
+    console.log('Login Usuario');
+    var formData = new FormData();
+    formData.append('username',document.getElementById('idUsuario').value);
+    formData.append('password',document.getElementById('idSenha').value);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST",
+                 "/login/",
+                 true);
+    xmlhttp.setRequestHeader("X-CSRFToken", csrfcookie());
+    xmlhttp.onreadystatechange = function () {
+        var resposta= JSON.parse(xmlhttp.responseText);
+        if(resposta.isValid) {
+            location.assign('http://127.0.0.1:8000/carteira/'+resposta.id);
+        } else {
+            alert(resposta.mensagem);
+        }
+    };
+    xmlhttp.send(formData);
+}
+
+function registrarUsuario() {
+    location.assign('http://127.0.0.1:8000/registrarusuario');
+}
+
+
+function temNomeUsuarioUrl() {
+    var urlAtual = window.location.href.toString().split('/');
+    var ultimoElemento = urlAtual[urlAtual.length-1];
+    console.log(ultimoElemento);
+    if( ultimoElemento != 'login' ) {
+        return ultimoElemento;
+    }
+    return false
+}
+
+function csrfcookie() {
+    var cookieValue = null,
+        name = 'csrftoken';
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};
